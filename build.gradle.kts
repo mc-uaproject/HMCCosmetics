45eb1fa6687a3e88be09968550b8c2e6bed468e6
lojosho <lojoshogaming@gmail.com>
2025-05-09 11:35:13 -0500

    chore: update HibiscusCommons

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,8 +69,6 @@ allprojects {
 
         // Hibiscus Commons
         maven("https://repo.hibiscusmc.com/releases")
-
-        mavenLocal()
     }
 
     dependencies {
@@ -88,7 +86,7 @@ allprojects {
         compileOnly("io.github.toxicity188:BetterCommand:1.3") //BetterCommand library
         //compileOnly("it.unimi.dsi:fastutil:8.5.14")
         compileOnly("org.projectlombok:lombok:1.18.34")
-        compileOnly("me.lojosho:HibiscusCommons:0.6.3-7ef40c1")
+        compileOnly("me.lojosho:HibiscusCommons:0.6.3-0f0baaf4")
 
         // Handled by Spigot Library Loader
         compileOnly("net.kyori:adventure-api:4.19.0")
