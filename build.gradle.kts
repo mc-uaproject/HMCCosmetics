7a6475c467ef410e374681606bc7c4d406c8a212
LoJoSho <LoJoShogaming@gmail.com>
2025-05-22 21:07:12 -0500

    chore: update LuckPerms

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,7 +139,7 @@ tasks {
         downloadPlugins {
             hangar("PlaceholderAPI", "2.11.6")
             url("https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar")
-            url("https://download.luckperms.net/1567/bukkit/loader/LuckPerms-Bukkit-5.4.150.jar")
+            url("https://download.luckperms.net/1582/bukkit/loader/LuckPerms-Bukkit-5.4.165.jar")
         }
     }
 
